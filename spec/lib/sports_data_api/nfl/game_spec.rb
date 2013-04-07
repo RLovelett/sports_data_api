2831d8c5e4f71f10e139208174e8d232e5354dda
Ryan Lovelett <ryan@lovelett.me>
2013-04-07 18:09:44 -0500

    Specs to describe the weekly schedule behavior

--- spec/lib/sports_data_api/nfl/game_spec.rb
+++ spec/lib/sports_data_api/nfl/game_spec.rb
@@ -15,6 +15,11 @@ describe SportsDataApi::Nfl::Game, vcr: {
     SportsDataApi.access_level = 't'
     SportsDataApi::Nfl.boxscore(2012, :REG, 9, 'IND', 'MIA')
   end
+  let(:weekly_schedule) do
+    SportsDataApi.access_level = 't'
+    SportsDataApi.key = api_key
+    SportsDataApi::Nfl.weekly(2012, :PRE, 1)
+  end
   context 'results from schedule fetch' do
     subject { season.weeks.first.games.first }
     it { should be_an_instance_of(SportsDataApi::Nfl::Game) }
@@ -25,6 +30,9 @@ describe SportsDataApi::Nfl::Game, vcr: {
     its(:status) { should eq 'closed' }
     its(:home_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
     its(:away_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
+    its(:venue) { should be_an_instance_of(SportsDataApi::Nfl::Venue) }
+    its(:broadcast) { should be_an_instance_of(SportsDataApi::Nfl::Broadcast) }
+    its(:weather) { should be_an_instance_of(SportsDataApi::Nfl::Weather) }
   end
   context 'results from boxscore fetch' do
     subject { boxscore }
@@ -36,6 +44,22 @@ describe SportsDataApi::Nfl::Game, vcr: {
     its(:status) { should eq 'closed' }
     its(:quarter) { should eq 4 }
     its(:clock) { should eq ':00' }
+    its(:home_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
+    its(:away_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
+    its(:venue) { should be_an_instance_of(SportsDataApi::Nfl::Venue) }
+    its(:broadcast) { should be_an_instance_of(SportsDataApi::Nfl::Broadcast) }
+    its(:weather) { should be_an_instance_of(SportsDataApi::Nfl::Weather) }
+  end
+  context 'results from weekly schedule fetch' do
+    subject { weekly_schedule.first }
+    it { should be_an_instance_of(SportsDataApi::Nfl::Game) }
+    its(:id) { should eq '433d3222-e82d-4f14-97e2-4115579473f6' }
+    its(:scheduled) { should eq Time.new(2012, 8, 9, 23, 00, 00, '+00:00') }
+    its(:home) { should eq 'BUF' }
+    its(:away) { should eq 'WAS' }
+    its(:status) { should eq 'closed' }
+    its(:quarter) { should eq 0 }
+    its(:clock) { should eq nil }
     its(:home_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
     its(:away_team) { should be_an_instance_of(SportsDataApi::Nfl::Team) }
   end
