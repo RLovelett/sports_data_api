88e3828f992b9d72c6c85fc2f8f7513ad7e59ac2
adam <adam.imberman@toplinegamelabs.com>
2014-09-05 14:37:24 -0700

    parse children elements for stats, get goaltending stats for nhl

--- lib/sports_data_api/stats.rb
+++ lib/sports_data_api/stats.rb
@@ -4,8 +4,12 @@ module SportsDataApi
       if xml.is_a? Nokogiri::XML::Element
         stat_ivar = self.instance_variable_set("@#{xml.name}", {})
         self.class.class_eval { attr_reader :"#{xml.name}" }
-        xml.attributes.each do | attr_name, attr_value|
-          stat_ivar[attr_name.to_sym] = attr_value.value
+        
+        xml.attributes.each { |attr_name, attr_value| stat_ivar[attr_name.to_sym] = attr_value.value }
+        xml.children.each do |child_stat|
+          if child_stat.is_a? Nokogiri::XML::Element
+            child_stat.attributes.each { |attr_name, attr_value| stat_ivar["#{child_stat.name}_#{attr_name}".to_sym] = attr_value.value }
+          end
         end
       end
     end
