dca4956cedd62b74f29e032d893412c2e8ce834f
Trevor John <trevor@playdraft.com>
2017-02-01 17:12:55 -0500

    remove unused data file

--- lib/sports_data_api/ncaafb/player.rb
+++ lib/sports_data_api/ncaafb/player.rb
@@ -1,13 +1,6 @@
 module SportsDataApi
   module Ncaafb
-    class Player
-      def initialize(json)
-        player_ivar = self.instance_variable_set("@player", {})
-        self.class.class_eval { attr_reader :"player" }
-        json.each_pair do | attr_name, attr_value|
-          player_ivar[attr_name.to_sym] = attr_value
-        end
-      end
+    class Player < SportsDataApi::JsonData
     end
   end
 end
