70330824cef198494a664030a9e05db9ade104d1
Trevor John <trevor@john.tj>
2017-03-14 15:48:15 -0400

    if method is defined in hash call method instead of accessing data

--- lib/sports_data_api/json_data.rb
+++ lib/sports_data_api/json_data.rb
@@ -1,22 +1,30 @@
 module SportsDataApi
   class JsonData
     def initialize(json)
-      attr_name = class_name
-      ivar = self.instance_variable_set("@#{attr_name}", {})
-      self.class.class_eval { attr_reader :"#{attr_name}" }
+      instance_var_name = class_name
+      ivar = self.instance_variable_set("@#{instance_var_name}", {})
+      self.class.class_eval { attr_reader :"#{instance_var_name}" }
       json.each_pair do |attr_name, attr_value|
         ivar[attr_name.to_sym] = attr_value
       end
     end
 
     def [](key)
+      return send(key) if respond_to?(key)
       self.instance_variable_get("@#{class_name}")[key]
     end
 
     private
 
     def class_name
-      @class_name ||= self.class.name.downcase.split('::').last
+      @class_name ||= self
+        .class
+        .name
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .split('::')
+        .last
+        .downcase
     end
   end
 end
