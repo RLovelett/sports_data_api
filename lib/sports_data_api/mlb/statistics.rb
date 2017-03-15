8c7702f2b0b06788618e69b6b6f294798b0d96a1
Trevor John <trevor@playdraft.com>
2017-03-15 13:50:02 -0400

    adds expected players to MLB Team

--- lib/sports_data_api/mlb/statistics.rb
+++ lib/sports_data_api/mlb/statistics.rb
@@ -2,22 +2,22 @@ module SportsDataApi
   module Mlb
     class Statistics < JsonData
       def pitching
-        @pitching ||= orphan_stat(:pitching, MergedStats)
+        @pitching ||= orphan_stat :pitching
       end
 
       def fielding
-        @fielding ||= orphan_stat(:fielding, MergedStats)
+        @fielding ||= orphan_stat :fielding
       end
 
       def hitting
-        @hitting ||= orphan_stat(:hitting, MergedStats)
+        @hitting ||= orphan_stat :hitting
       end
 
       private
 
-      def orphan_stat(key, klass)
+      def orphan_stat(key)
         return unless statistics.has_key? key
-        klass.new(statistics[key]['overall'], key.to_s)
+        MergedStats.new(statistics[key]['overall'], key.to_s)
       end
     end
   end
