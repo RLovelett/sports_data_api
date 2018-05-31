74bcbf254aa718fdd99a4738684935b16fb261be
Trevor John <trevor@john.tj>
2018-05-31 12:06:38 -0400

    Save specs to file for running only failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+  config.example_status_persistence_file_path = 'tmp/specs.txt'
 end
 
 VCR.configure do |c|
